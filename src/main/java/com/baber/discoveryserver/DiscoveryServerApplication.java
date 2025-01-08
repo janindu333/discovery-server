c8ef565a3e1a288d00e095e75b795d5ddf71f76e
janindu <janindu.praneeth@gmail.com>
2025-01-08 11:31:28 +0530

    update ci cd pipeline

--- src/main/java/com/baber/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/baber/discoveryserver/DiscoveryServerApplication.java
@@ -7,8 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
-    public static void main(String args[]) {
-        System.out.println();
+    public static void main(String args[]) { 
         SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 
